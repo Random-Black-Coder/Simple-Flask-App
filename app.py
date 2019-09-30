74285f394f9acfb780aa3d8b6f7f8d37a3fe75b0
unknown <yahkeef@gmail.com>
2019-09-29 20:32:08 -0400

    Added delte patient functionality

--- app.py
+++ app.py
@@ -2,14 +2,22 @@
 from flask import Flask, request, flash, url_for, redirect, render_template
 from flask_wtf import FlaskForm
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+from flask_nav import Nav
+from flask_nav.elements import Navbar, Subgroup, View, Link, Text, Separator
 
 #initialize app
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'you-will-never-guess'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:root@localhost/Patients'
 db = SQLAlchemy(app)
+nav = Nav(app)
+Bootstrap(app)
 
-#patient oject
+#build navbar
+nav.register_element('my_navbar', Navbar('thenav', View('Home Page','show_all')))
+
+#patient Database object
 class Patients(db.Model):
     __tablename__ = 'patients'
     requesterName = db.Column(db.String(50))
@@ -18,6 +26,7 @@ class Patients(db.Model):
     dateReceived = db.Column(db.Date)
     comments = db.Column(db.String(60))
 
+    #patient constructor
     def __init__(self, requesterName, patientName, medicalRecordNumber, dateReceived, comments):
         self.requesterName = requesterName
         self.patientName = patientName
@@ -47,6 +56,22 @@ def new():
          return redirect(url_for('show_all'))
    return render_template('new.html')
 
+#patient deletion form
+@app.route('/delete', methods = ['GET', 'POST'])
+def delete():
+   if request.method == 'POST':
+      if not request.form['medicalRecordNumber']:
+         flash('Please enter the patients medical record number.', 'error')
+      else:
+          mrn = request.form['medicalRecordNumber']
+          patient = Patients.query.filter_by(medicalRecordNumber=mrn).first()
+
+          db.session.delete(patient)
+          db.session.commit()
+
+          flash('Record was successfully removed')
+          return redirect(url_for('show_all'))
+   return render_template('delete.html')
 
 if __name__ == '__main__':
     db.create_all()
